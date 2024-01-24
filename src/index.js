b954696a11ef4ab3341750c1f996ad79f8ffe48b
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2024-01-23 21:51:50 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 module.exports = app;
