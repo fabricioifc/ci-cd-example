9d79e00fd4d33e2791e92c56b8ac2e762a26429f
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2024-01-24 09:47:59 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 })
 
