65daa94a0728adaccd0e27a73c2460ef5c7383a3
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2024-01-24 09:35:36 -0300

    teste

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  target: 'node',
+  mode: 'production',
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
@@ -10,6 +13,20 @@ module.exports = {
   resolve: {
     extensions: ['.js'],
   },
-  target: 'node',
-  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+  ],
 }
