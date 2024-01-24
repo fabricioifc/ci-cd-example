489409070d5740472f294ec15ceab07875aabb46
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2024-01-24 09:00:58 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.status(200).json({ message: 'Hello World!' });
 })
 
 app.listen(port, () => {
